9d84074c83e0c0c0a073b1c918c85c2c47c463be
Alona Korzhnev <alonakorzhnev@gmail.com>
2019-09-23 15:25:35 +0300

    Add files via upload

--- C++/String_t/string_t.cpp
+++ C++/String_t/string_t.cpp
@@ -1,31 +1,30 @@
 #include "string_t.h"
 #include <string.h>
-#include <iostream>
-using namespace std;
+#define MAX_SIZE 64
 
-char* String_t::createFrom(const char* str)
+char *String_t::createFrom(const char *str)
 {
-    char* tmp = new char[strlen(str) + 1];
+    char *tmp = new char[strlen(str) + 1];
     strcpy(tmp, str);
     return tmp;
 }
 
 String_t::String_t()
 {
-    this->str = 0;
+    this->str = createFrom("");
     this->lenght = 0;
 }
 
-String_t::String_t(const char* str)
+String_t::String_t(const char *str)
 {
-    if(str != NULL)
+    if (str != NULL)
     {
         this->str = createFrom(str);
         this->lenght = strlen(this->str);
     }
 }
 
-String_t::String_t(String_t& s)
+String_t::String_t(String_t &s)
 {
     this->lenght = s.lenght;
     this->str = createFrom(s.str);
@@ -36,9 +35,9 @@ String_t::~String_t()
     delete[] this->str;
 }
 
-String_t& String_t::operator=(const String_t& s)
+String_t &String_t::operator=(const String_t &s)
 {
-    if(this != &s)
+    if (this != &s)
     {
         delete[] this->str;
         this->str = createFrom(s.str);
@@ -51,9 +50,9 @@ const int String_t::getLenght() const
     return this->lenght;
 }
 
-void String_t::setString(const char* str)
+void String_t::setString(const char *str)
 {
-     if(str != NULL)
+    if (str != NULL)
     {
         delete[] this->str;
         this->str = createFrom(str);
@@ -61,43 +60,137 @@ void String_t::setString(const char* str)
     }
 }
 
-const char* String_t::getString() const
+const char *String_t::getString() const
 {
     return this->str;
 }
 
-int String_t::stringCompare(const String_t& s) const
+int String_t::stringCompare(const String_t &s) const
 {
     int result = strcmp(this->str, s.str);
 
-    if(result < 0)
+    if (result < 0)
     {
         return 1;
     }
-    else if(result > 0)
+    else if (result > 0)
     {
         return 2;
     }
     else
     {
         return 0;
-    }    
+    }
 }
 
 void String_t::print() const
 {
-    if(this->str != NULL)
-    {
-        cout << "String: ";
-        for(int i = 0; i < this->lenght; i++)
-        {
-        cout << this->str[i];
-        }
-    }
-    else
-    {
-        cout << "String: NULL";
-    }
-    
-    cout << " lenght: " << this->lenght << endl;
+    cout << "String: " << this->str;
 }
+
+void String_t::toUpper()
+{
+    for (int i = 0; i < this->lenght; ++i)
+    {
+        this->str[i] = toupper(this->str[i]);
+    }
+}
+
+void String_t::toLower()
+{
+    for (int i = 0; i < this->lenght; ++i)
+    {
+        this->str[i] = tolower(this->str[i]);
+    }
+}
+
+void String_t::prepend(const char *str)
+{
+    char* tmp = new char[strlen(this->str) + strlen(str) + 1];
+    strcpy(tmp, str);
+    strcat(tmp, this->str);
+    delete[] this->str;
+    this->str = createFrom(str);
+    this->lenght = strlen(this->str);
+    delete[] tmp;
+}
+
+void String_t::prepend(const String_t &s)
+{
+    prepend(s.str);
+}
+
+String_t& String_t::operator+=(const char *str)
+{
+    char* tmp = new char[strlen(this->str) + strlen(str) + 1];
+    strcpy(tmp, this->str);
+    strcat(tmp, str);
+    delete[] this->str;
+    this->str = createFrom(str);
+    this->lenght = strlen(this->str);
+    delete[] tmp;
+}
+
+String_t& String_t::operator+=(const String_t &s)
+{
+    return *this += s.str;
+}
+
+bool String_t::operator>(const String_t &s)
+{
+    return (stringCompare(s) == 2) ? true : false;    
+}
+
+bool String_t::operator<(const String_t &s)
+{
+    return (stringCompare(s) == 1) ? true : false;    
+}
+
+bool String_t::operator>=(const String_t &s)
+{
+    return (stringCompare(s) == 2 || stringCompare(s) == 0) ? true : false;    
+}
+
+bool String_t::operator<=(const String_t &s)
+{
+    return (stringCompare(s) == 1 || stringCompare(s) == 0) ? true : false;
+}
+
+bool String_t::operator==(const String_t &s)
+{
+    return (stringCompare(s) == 0) ? true : false;   
+}
+
+bool String_t::operator!=(const String_t &s)
+{
+    return (stringCompare(s) != 0) ? true : false;   
+}
+
+bool String_t::contains(const char *str) const
+{
+    return strstr(this->str, str);
+}
+
+char& String_t::operator[](size_t index)
+{
+    return (index > 0 && index < this->lenght) ? this->str[index] : this->str[this->lenght];
+}
+
+char String_t::operator[](size_t index) const
+{
+    return (index > 0 && index < this->lenght) ? this->str[index] : this->str[this->lenght];    
+}
+
+ostream& operator<<(ostream& os, const String_t& s)
+{
+    os << s.getString();
+    return os;
+}
+
+istream& operator>>(istream& is, String_t& s)
+{
+    char str[MAX_SIZE];
+    is >> str;
+    s.setString(str);
+}
+
--- C++/String_t/string_t.h
+++ C++/String_t/string_t.h
@@ -1,22 +1,51 @@
-#pragma once
+#ifndef STRING_T
+#define STRING_T
+#include <iostream>
+using namespace std;
 
-class  String_t
+class String_t
 {
-private:
-    char* str;
-    int lenght;
-    char* createFrom(const char* str);
-
 public:
-    String_t();
-    String_t(const char* str);
-    String_t(String_t& s);
     ~String_t();
-    String_t& operator=(const String_t& s);
+    String_t();
+    String_t(const char *str);
+    String_t(String_t &s);
+    String_t &operator=(const String_t &s);
 
     const int getLenght() const;
-    void setString(const char* str);
-    const char* getString() const;
-    int stringCompare(const String_t& s) const;
+    void setString(const char *str);
+    const char *getString() const;
+    int stringCompare(const String_t &s) const;
     void print() const;
+
+    void toUpper();
+    void toLower();
+
+    void prepend(const char *str);
+    void prepend(const String_t &s);
+
+    String_t& operator+=(const char *str);
+    String_t& operator+=(const String_t &s);
+
+    bool operator>(const String_t &s);
+    bool operator<(const String_t &s);
+    bool operator>=(const String_t &s);
+    bool operator<=(const String_t &s);
+    bool operator==(const String_t &s);
+    bool operator!=(const String_t &s);
+
+    bool contains(const char *str) const;
+
+    char& operator[](size_t index);
+    char operator[](size_t index) const;
+
+private:
+    char *str;
+    int lenght;
+    char *createFrom(const char *str);
 };
+
+ostream& operator<<(ostream& os, const String_t &s);
+istream& operator>>(istream& is, String_t &s);
+
+#endif
