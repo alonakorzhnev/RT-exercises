a367cf26c5a483ed1be47466bf6fe98e4d0b94d5
SQRT <alonakorzhnev@gmail.com>
2019-10-06 09:51:42 +0300

    exception

--- C++/Exception_handler/exception_t.h
+++ C++/Exception_handler/exception_t.h
@@ -7,11 +7,22 @@ template <class T>
 class TException_t
 {
     public:
-        TException_t(T exception, string message, string fileName, int lineNumber);
+        ~TException_t() {}
+        TException_t(const T& exception, const string& message, const string& fileName, int lineNumber):
+                m_exception(exception), m_message(message), 
+                m_fileName(fileName), m_lineNumber(lineNumber) {}
+
+        TException_t(const TException_t& ex) 
+        {   
+            m_exception = ex.m_exception;
+            m_message = ex.m_message;
+            m_fileName = ex.m_fileName;
+            m_lineNumber = ex.m_lineNumber;
+        }
 
         const T& getException() const {return m_exception;}
-        string getMessage() const {return m_message;}
-        string getFileName() const {return m_fileName;}
+        const string& getMessage() const {return m_message;}
+        const string& getFileName() const {return m_fileName;}
         int getLineNumber() const {return m_lineNumber;}
 
     private:
@@ -19,14 +30,8 @@ class TException_t
         string m_message;
         string m_fileName;
         int m_lineNumber;
+
+        TException_t& operator=(const TException_t& ex);
 };
 
-template <class T>
-TException_t<T>::TException_t(T exception, string message, string fileName, int lineNumber)
-{
-    m_exception = exception;
-    m_message = message;
-    m_fileName = fileName;
-    m_lineNumber = lineNumber;
-}
 
